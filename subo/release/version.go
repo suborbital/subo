abf2b7f0a4b86c1fe0c55e110aa3cdd25140ba72
Connor Hicks <connorjhicks@gmail.com>
2021-01-06 08:48:51 -0500

    update comment

--- subo/release/version.go
+++ subo/release/version.go
@@ -1,6 +1,7 @@
 package release
 
 // SuboDotVersion represents the dot version for subo
+// it is also the image tag used for builders
 var SuboDotVersion = "0.0.7"
 
 // FFIVersion is the FFI version used by this version of subo
