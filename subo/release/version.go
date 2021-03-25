b5e8139bb7c417bc4c560160b05e66c930104ecc
Connor Hicks <connorjhicks@gmail.com>
2021-03-24 21:37:58 -0400

    added changelog and bumped version

--- subo/release/version.go
+++ subo/release/version.go
@@ -2,7 +2,7 @@ package release
 
 // SuboDotVersion represents the dot version for subo
 // it is also the image tag used for builders
-var SuboDotVersion = "0.0.14"
+var SuboDotVersion = "0.0.15"
 
 // FFIVersion is the FFI version used by this version of subo
 var FFIVersion = "0.9.1"
