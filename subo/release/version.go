209bfe13bf492b187dd2664f35a134cdc3d26885
cohix <connor@suborbital.dev>
2021-06-14 23:24:26 -0400

    update API version and AS template to use API version

--- subo/release/version.go
+++ subo/release/version.go
@@ -5,7 +5,7 @@ package release
 var SuboDotVersion = "0.0.15"
 
 // FFIVersion is the FFI version used by this version of subo
-var FFIVersion = "0.9.1"
+var FFIVersion = "0.10.0"
 
 // AtmoVersion is the default version of Atmo that will be used for new projects
 var AtmoVersion = "0.2.2"
