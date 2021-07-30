7acf53f54bd06139fb065e0e6d9d236afdcd4e94
Jagger De Leo <jcdl@fastmail.com>
2021-07-30 18:27:42 -0400

    CLI: Display help when running with no arguments
    Closes #80.

--- subo/main.go
+++ subo/main.go
@@ -2,16 +2,10 @@ package main
 
 import (
 	"os"
-
-	"github.com/spf13/cobra"
 )
 
 func main() {
 	rootCmd := rootCommand()
-
-	rootCmd.RunE = func(cmd *cobra.Command, args []string) error {
-		return nil
-	}
 
 	if err := rootCmd.Execute(); err != nil {
 		os.Exit(-1)
