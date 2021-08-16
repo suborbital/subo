40a83f505497c07ec4614ffe47442642db11df1f
cohix <connor@suborbital.dev>
2021-08-16 11:44:40 -0400

    bump versions and add changelog for v0.0.21

--- subo/release/version.go
+++ subo/release/version.go
@@ -2,7 +2,7 @@ package release
 
 // SuboDotVersion represents the dot version for subo
 // it is also the image tag used for builders
-var SuboDotVersion = "0.0.20"
+var SuboDotVersion = "0.0.21"
 
 // FFIVersion is the FFI version used by this version of subo
 var FFIVersion = "0.11.1"
@@ -10,4 +10,5 @@ var FFIVersion = "0.11.1"
 // AtmoVersion is the default version of Atmo that will be used for new projects
 var AtmoVersion = "0.3.1"
 
-var SCCTag = "v0.0.3"
+// SCCTag is the docker tag used for creating new compute core deployments
+var SCCTag = "v0.0.4"
