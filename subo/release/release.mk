3861a202ba9766c61e63ef060d0b97a06a7de6d6
Jagger De Leo <jagger@suborbital.dev>
2021-11-24 20:24:56 -0500

    Add .git directory check to release Makefile
    If Subo is built without a .git folder present, set CommitHash to
    "unknown"

--- subo/release/release.mk
+++ subo/release/release.mk
@@ -1,5 +1,5 @@
 now = $(shell date +'%Y-%m-%dT%TZ')
-commit = $(shell git rev-parse HEAD)
+commit = $(shell if [ ! -d .git ]; then echo "unknown"; else git rev-parse HEAD; fi)
 var_path = github.com/suborbital/subo/subo/release
 RELEASE_FLAGS = "-X $(var_path).CommitHash=$(commit)\
  -X $(var_path).BuildTime=$(now)"
