9e3ea5e03425a6bc7db1afdea64aa5e6e70de0c4
Super Nova <60084237+MacklinEngineering@users.noreply.github.com>
2022-01-24 15:35:28 -0500

    feat(subo): subo create handler command

--- subo/command/create_handler.go
+++ subo/command/create_handler.go
@@ -16,17 +16,9 @@ type handlerData struct {
 	Method      string
 }
 
-//GOAL
-// Would love to be able to run subo create handler /foo and have it added to the directive.
-// The basic command can just create a handler with a placeholder function.
-// Some potential flags: --method, --stream, --steps
-
-//READ ENTIRE FILE
-// Write CreateHandler Function
-// CreateHandlerCmd returns the build command
 func CreateHandlerCmd() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "handler <name>", //this <name> has to pull a default runnable which is in form: /name
+		Use:   "handler <name>",
 		Short: "create a new handler",
 		Long:  `create a new handler for Subo CLI`,
 		Args:  cobra.ExactArgs(1),
@@ -36,8 +28,7 @@ func CreateHandlerCmd() *cobra.Command {
 			handlerType, _ := cmd.Flags().GetString(typeFlag)
 			resource, _ := cmd.Flags().GetString(resourceFlag) //name and the resource are the same
 			method, _ := cmd.Flags().GetString(methodFlag)
-			// stream, _ := cmd.Flags().GetString(streamFlag)
-			// steps, _ := cmd.Flags().GetString(stepsFlag)
+
 			dir, _ := cmd.Flags().GetString(dirFlag)
 
 			util.LogStart(fmt.Sprintf("creating handler with function name %s", name))
@@ -51,8 +42,7 @@ func CreateHandlerCmd() *cobra.Command {
 				util.LogFail("Handlers must be created in a project")
 				return errors.New("Directive.yaml not found")
 			}
-			// //create new handler object
-
+			//Create a new handler object
 			handler := directive.Handler{
 				Input: directive.Input{
 					Type:     handlerType,
@@ -61,10 +51,7 @@ func CreateHandlerCmd() *cobra.Command {
 				},
 			}
 
-			// Stream:     stream,
-			// Steps:  	steps,
-
-			//add handler object to the directive
+			//Add the handler object to the directive file
 			bctx.Directive.Handlers = append(bctx.Directive.Handlers, handler)
 
 			//Write Directive File which overwrites the entire file
@@ -79,111 +66,8 @@ func CreateHandlerCmd() *cobra.Command {
 	}
 
 	cmd.Flags().String(typeFlag, "request", "the method for which you want ")
-	cmd.Flags().String(resourceFlag, "/foo", "git branch to download templates from") //
+	cmd.Flags().String(resourceFlag, "/foo", "git branch to download templates from")
 	cmd.Flags().String(methodFlag, "GET", "the method for which you want ")
-	// cmd.Flags().String(streamFlag, "main", "git branch to download templates from") //stream is a stype of handler
-	// cmd.Flags().String(stepsFlag, "fn", "Runnable functions to be composed when handling requests to the resource.")
 
 	return cmd
 }
-
-// // WriteDirectiveFile writes a Directive to disk
-// func WriteDirectiveFile(cwd string, directive *directive.Directive) error {
-// 	filePath := filepath.Join(cwd, "Directive.yaml")
-
-// 	directiveBytes, err := yaml.Marshal(directive)
-// 	if err != nil {
-// 		return errors.Wrap(err, "failed to Marshal")
-// 	}
-
-// 	if err := ioutil.WriteFile(filePath, directiveBytes, os.FileMode(os.O_WRONLY)); err != nil {
-// 		return errors.Wrap(err, "failed to WriteFile")
-// 	}
-
-// 	return nil
-// }
-
-//Writes the handler
-// func writeHandler(cwd string, handlerType, resource, method, string) (*directive.Runnable, error) { //notate optional params      , method, lang, namespace string
-
-// 	handler := &directive.Handler{
-// 		HandlerType:       handlerType,
-// 		Resource:   resource,
-// 		Method:     method,
-// 		// Stream:     stream,
-// 		// Steps:  	steps,
-// 	}
-
-// 	//Not sure if I need this at all
-// 	bytes, err := yaml.Marshal(handler)
-// 	if err != nil {
-// 		return nil, errors.Wrap(err, "failed to Marshal handler")
-// 	}
-
-// 	path := filepath.Join(cwd, name, ".handler.yaml") //How to get it to Directive.yaml? Is this being appended to the directive.yaml.tmpl file?
-
-// 	if err := ioutil.WriteFile(path, bytes, 0700); err != nil { //Don't need to write entire file
-// 		return nil, errors.Wrap(err, "failed to WriteFile handler")
-// 	}
-
-// 	return handler, nil
-// }
-
-// func appendHandler(cwd, method, stream, steps string) (*directive.Runnable, error) {
-
-// }
-
-//NOTES
-
-//Handler Structure
-// handlers:
-//   - type: request
-//     resource: /hello
-//     method: POST
-//     steps:
-//       - group:
-//         - fn: modify-url
-//         - fn: helloworld-rs
-//           as: hello
-//       - fn: fetch-test
-//         with:
-//           url: modify-url
-//           logme: hello
-
-//
-//
-//					IN ATMO
-//
-// // Handlers returns the handlers for the app
-// func (h *HeadlessBundleSource) Handlers() []directive.Handler {
-// 	if h.bundlesource.bundle == nil {
-// 		return []directive.Handler{}
-// 	}
-
-// 	handlers := []directive.Handler{}
-
-// 	// for each Runnable, construct a handler that executes it
-// 	// based on a POST request to its FQFN URL /identifier/namespace/fn/version
-// 	for _, runnable := range h.bundlesource.Runnables() {
-// 		handler := directive.Handler{
-// 			Input: directive.Input{
-// 				Type:     directive.InputTypeRequest,
-// 				Method:   http.MethodPost,
-// 				Resource: fqfn.Parse(runnable.FQFN).HeadlessURLPath(),
-// 			},
-// 			Steps: []executable.Executable{
-// 				{
-// 					CallableFn: executable.CallableFn{
-// 						Fn:   runnable.Name,
-// 						With: map[string]string{},
-// 						FQFN: runnable.FQFN,
-// 					},
-// 				},
-// 			},
-// 		}
-
-// 		handlers = append(handlers, handler)
-// 	}
-
-// 	return handlers
-// }
