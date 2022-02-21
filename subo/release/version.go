fbf69c9994c5f46a8310ff637a2906643ef2952a
Oscar Spencer <oscar@grain-lang.org>
2022-02-21 11:53:24 -0500

    chore: Upgrade to Atmo Beta-4.3 and Reactr Beta-15

--- subo/release/version.go
+++ subo/release/version.go
@@ -5,10 +5,10 @@ package release
 var SuboDotVersion = "0.3.2"
 
 // FFIVersion is the FFI version used by this version of subo.
-var FFIVersion = "0.13.1"
+var FFIVersion = "0.15.0"
 
 // AtmoVersion is the default version of Atmo that will be used for new projects.
-var AtmoVersion = "0.4.2"
+var AtmoVersion = "0.4.3"
 
 // SCCTag is the docker tag used for creating new compute core deployments.
 var SCCTag = "v0.1.1"
