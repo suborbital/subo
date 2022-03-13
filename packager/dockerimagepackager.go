6672a187f3fcc50c49608b4da986c8aa51cdca17
cohix <connor@suborbital.dev>
2022-03-12 21:09:47 -0500

    add docker push option

--- packager/dockerimagepackager.go
+++ packager/dockerimagepackager.go
@@ -3,9 +3,11 @@ package packager
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/pkg/errors"
 
+	"github.com/suborbital/atmo/directive"
 	"github.com/suborbital/subo/project"
 	"github.com/suborbital/subo/subo/util"
 )
@@ -35,13 +37,32 @@ func (b *DockerImagePackageJob) Package(log util.FriendlyLogger, ctx *project.Co
 		return errors.New("missing project bundle")
 	}
 
+	imageName, err := dockerNameFromDirective(ctx.Directive)
+	if err != nil {
+		return errors.Wrap(err, "failed to dockerNameFromDirective")
+	}
+
 	os.Setenv("DOCKER_BUILDKIT", "0")
 
-	if _, err := util.Run(fmt.Sprintf("docker build . -t=%s:%s", ctx.Directive.Identifier, ctx.Directive.AppVersion)); err != nil {
+	if _, err := util.Run(fmt.Sprintf("docker build . -t=%s", imageName)); err != nil {
 		return errors.Wrap(err, "🚫 failed to build Docker image")
 	}
 
-	util.LogDone(fmt.Sprintf("built Docker image -> %s:%s", ctx.Directive.Identifier, ctx.Directive.AppVersion))
+	util.LogDone(fmt.Sprintf("built Docker image -> %s", imageName))
 
 	return nil
+}
+
+func dockerNameFromDirective(d *directive.Directive) (string, error) {
+	identParts := strings.Split(d.Identifier, ".")
+	if len(identParts) != 3 {
+		return "", errors.New("ident has incorrect number of parts")
+	}
+
+	org := identParts[1]
+	repo := identParts[2]
+
+	name := fmt.Sprintf("%s/%s:%s", org, repo, d.AppVersion)
+
+	return name, nil
 }
---
+++ packager/dockerpublisher.go
@@ -0,0 +1,63 @@
+package packager
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/pkg/errors"
+	"github.com/suborbital/subo/project"
+	"github.com/suborbital/subo/subo/util"
+)
+
+const (
+	dockerPublishJobType = "docker"
+)
+
+type DockerPublishJob struct{}
+
+// NewDockerPublishJob returns a new PublishJob for Bindle.
+func NewDockerPublishJob() PublishJob {
+	d := &DockerPublishJob{}
+
+	return d
+}
+
+// Type returns the publish job's type.
+func (b *DockerPublishJob) Type() string {
+	return dockerPublishJobType
+}
+
+// Publish publishes the application.
+func (b *DockerPublishJob) Publish(log util.FriendlyLogger, ctx *project.Context) error {
+	if ctx.Directive == nil {
+		return errors.New("cannot publish without Directive.yaml")
+	}
+
+	if !ctx.Bundle.Exists {
+		return errors.New("cannot publish without runnables.wasm.zip, run `subo build` first")
+	}
+
+	imagesCmd := "docker images --format '{{ .Repository }}:{{ .Tag }}'"
+
+	imagesOutput, err := util.RunSilent(imagesCmd)
+	if err != nil {
+		return errors.Wrap(err, "failed to Run images command")
+	}
+
+	imageName, err := dockerNameFromDirective(ctx.Directive)
+	if err != nil {
+		return errors.Wrap(err, "failed to dockerNameFromDirective")
+	}
+
+	if !strings.Contains(imagesOutput, imageName) {
+		return fmt.Errorf("image %s not found, run `subo build --docker` first", imageName)
+	}
+
+	if _, err := util.Run(fmt.Sprintf("docker push %s", imageName)); err != nil {
+		return errors.Wrap(err, "failed to Run docker push")
+	}
+
+	util.LogDone(fmt.Sprintf("pushed Docker image -> %s", imageName))
+
+	return nil
+}
