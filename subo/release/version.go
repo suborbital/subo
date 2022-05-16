c695f180bdfa26226717eedd457b24bc209a4cb2
Jagger De Leo <jagger@suborbital.dev>
2022-05-16 16:32:45 -0400

    chore(release): Bump version numbers

--- subo/release/version.go
+++ subo/release/version.go
@@ -2,13 +2,13 @@ package release
 
 // SuboDotVersion represents the dot version for subo
 // it is also the image tag used for builders.
-var SuboDotVersion = "0.4.2"
+var SuboDotVersion = "0.5.0"
 
 // FFIVersion is the FFI version used by this version of subo.
 var FFIVersion = "0.15.1"
 
 // AtmoVersion is the default version of Atmo that will be used for new projects.
-var AtmoVersion = "0.4.4"
+var AtmoVersion = "0.4.7"
 
 // SCCTag is the docker tag used for creating new compute core deployments.
 var SCCTag = "v0.2.0"
