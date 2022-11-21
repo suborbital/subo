ad857f29ceec0089cbbf06b508e36f73a382076b
Dylan Arbour <arbourd@users.noreply.github.com>
2022-11-21 10:25:06 -0500

    chore: Update subo to use SE2 images over SCC/SCN

--- subo/release/version.go
+++ subo/release/version.go
@@ -10,5 +10,5 @@ var FFIVersion = "0.15.1"
 // RuntimeVersion is the default version of E2Core that will be used for new projects.
 var RuntimeVersion = "0.4.7"
 
-// SCCTag is the docker tag used for creating new SE2 core deployments.
-var SCCTag = "v0.3.1"
+// SE2Tag is the docker tag used for creating new SE2 deployments.
+var SE2Tag = "v0.4.1"
